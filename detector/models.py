29e1a0237811f633272abea06021b030e1ce32bf
bharathi021 <bharathisarath.2@gmail.com>
2025-05-30 20:41:06 +0530

    changes happend added model

--- detector/models.py
+++ detector/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User # type: ignore
 
 class NewsArticle(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, default='')  # new field added
     text = models.TextField()
     is_fake = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
